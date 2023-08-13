be3e66e978456d8bf28b630a3f1e02bc32d7893b
Potassium <brainiacgamer444@gmail.com>
2023-08-13 18:59:13 +0545

    Create two divs and append them in a parent div

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const sketchBox = document.createElement("div");
+const menuBar = document.createElement("div");
+
+//menuBar
+menuBar.classList.add("menu");
+container.appendChild(menuBar);
+
+//sketchBox
+sketchBox.classList.add("sketchBox");
+container.appendChild(sketchBox);
